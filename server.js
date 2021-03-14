565a06aca4fb9b34a72b4ae9dd5c923b64751b45
edgarjdq <edgarjdq@gmail.com>
2021-03-14 16:06:57 -0300

    Base template

---
+++ server.js
@@ -0,0 +1,21 @@
+
+// start up
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var server = http;
+app.set('port', (process.env.PORT || 3333));
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', function(request, response) {
+	response.sendFile('index.html', {
+		root : __dirname
+	});
+});
+app.get('*', function(request, response) {
+	response.sendFile('404.html', {
+		root : __dirname
+	});
+});
+http.listen(app.get('port'), function() {
+	console.log('listening on ' + app.get('port'));
+});
