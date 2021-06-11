57c917b9916a2147ce59ff8bea7a8336491db3cb
edgarjdq <edgarjdq@gmail.com>
2021-06-11 14:47:41 -0300

    Added global stuff

--- public/js/client.js
+++ public/js/client.js
@@ -1,9 +1,36 @@
 var socket = io.connect();
+var getVideo = document.getElementById("pirata");
+
+function resumeAll() {
+    socket.emit('resume_all', position)
+}
 
 function pauseAll() {
     socket.emit('pause_all', {})
 }
 
-socket.on('algo', function(data){
-    //update ui
+function changeSource(vid) {
+    var source = document.getElementById("fuente");
+    source.setAttribute('src', vid);
+    getVideo.load()
+}
+
+function openFullscreen() {
+    var elem = document.getElementById("pirata");
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+socket.on('resume', function(position){
+    getVideo.currentTime = position
+    getVideo.play();
+});
+
+socket.on('pause', function(){
+    getVideo.pause();
 });
